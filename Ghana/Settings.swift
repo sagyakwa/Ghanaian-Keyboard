fbfc1d6c31812ef40ccade674f77643bfae28361
Samuel Agyakwa <samuelagyen@gmail.com>
2020-05-18 21:44:29 -0400

    Added an ObservableObject subclass for settings items

---
+++ Ghana/Settings.swift
@@ -0,0 +1,25 @@
+//
+//  Settings.swift
+//  Ghanaian Keyboard
+//
+//  Created by Samuel Agyakwa on 5/18/20.
+//  Copyright © 2020 Tetra Apps. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+class Settings: ObservableObject {
+    var didChange = PassthroughSubject<Void, Never>()
+    
+    @Published var isBluetoothOn = false { didSet { update() } }
+    
+    @Published var types = ["Off","On"]
+    @Published var type = 0 { didSet { update() } }
+    
+    @Published var isToggleOn = false { didSet { update() } }
+    
+    func update() {
+        didChange.send(())
+    }
+}
