a7653bbf5957f46c76e0da91249f9dfe6523aa5e
Samuel Agyakwa <samuelagyen@gmail.com>
2020-05-20 13:42:10 -0400

    added a view controller holder for presenting a view full screen without navigation or a sheet

---
+++ Ghana Keyboard/ViewControllerHolder.swift
@@ -0,0 +1,42 @@
+//
+//  ViewControllerHolder.swift
+//  Ghana Keyboard
+//
+//  Created by Samuel Agyakwa on 5/20/20.
+//  Copyright © 2020 Tetra Apps. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct ViewControllerHolder {
+    weak var value: UIViewController?
+}
+
+
+struct ViewControllerKey: EnvironmentKey {
+    static var defaultValue: ViewControllerHolder {
+        return ViewControllerHolder(value: UIApplication.shared.windows.first?.rootViewController)
+
+    }
+}
+
+
+extension EnvironmentValues {
+    var viewController: UIViewController? {
+        get { return self[ViewControllerKey.self].value }
+        set { self[ViewControllerKey.self].value = newValue }
+    }
+}
+
+extension UIViewController {
+    func present<Content: View>(style: UIModalPresentationStyle = .automatic, @ViewBuilder builder: () -> Content) {
+        let toPresent = UIHostingController(rootView: AnyView(EmptyView()))
+        toPresent.modalPresentationStyle = style
+        toPresent.rootView = AnyView(
+            builder()
+                .environment(\.viewController, toPresent)
+        )
+        self.present(toPresent, animated: true, completion: nil)
+    }
+}
